ee4d84fd7281735e09cc3efdc858eb7e81cdc606
Jovanyashraf123 <jovanywork12@gmail.com>
2026-09-16 06:13:52 +0300

    Add PWA manifest and icons3

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,8 @@ const FILES_TO_CACHE = [
   "./main.css",
   "./main.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 // تثبيت الـ service worker وتخزين الملفات الأساسية
